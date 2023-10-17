18d9557f3265b6d64c403b931d707b69d187a825
Biontos <116175438+Biontos@users.noreply.github.com>
2023-10-17 19:58:57 +0300

    Add files via upload

---
+++ 38.cs
@@ -0,0 +1,68 @@
+﻿////задание 1 стр 38
+//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//int sum = 0;
+//foreach (int number in sequence)
+//{
+//    sum += number;
+//}
+//Console.WriteLine($"Сумма элементов последовательности: {sum}");
+
+////задание 4 стр 38
+//int[] vso = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//int tarNum = 1;
+//int count = 0;
+//foreach (int num in vso)
+//{
+//    if (num == tarNum)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine($"Число {tarNum} встречается в последовательности {count} раз ");
+
+//// задание 6 стр 38
+//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//int product = 1;
+//foreach (int number in sequence)
+//{
+//    product *= number;
+//}
+//Console.WriteLine($"Произведение элементов последовательности: {product}");
+
+////задание 7 стр 38
+//int[] sequence = { -1, 2, -3, 4, -5, 6, -7, 8, -9 };
+//int sum = 0;
+//foreach (int number in sequence)
+//{
+//    sum += Math.Abs(number);
+//}
+//Console.WriteLine($"Сумма модулей элементов последовательности: {sum}");
+
+////задание 9 стр 38
+//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//int sum = 0;
+//foreach (int number in sequence)
+//{
+//    sum += number*number;
+//}
+//Console.WriteLine($"Сумма квадратов элементов последовательности: {sum}");
+
+//задание 10 стр 38
+//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//int sum = 0;
+//int sum1 = 0;
+//foreach (int number in sequence)
+//{
+//    sum += number;
+//    sum1 = sum / 9;
+//}
+//Console.WriteLine($"Среднее арифметическое последовательности: {sum1}");
+
+////задание 12 стр 38
+//int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//int sum = -1;
+//foreach (int number in sequence)
+//{
+//    sum *= number * number;
+//}
+//Console.WriteLine($"Сумма произведеней элементов последовательности: {sum}");
